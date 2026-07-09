fbd70cf41c41186ba91410fed5e6e60868383e7b
LaishaChavez <laishayalvanychavez@gmail.com>
2026-07-08 21:47:32 -0700

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
+namespace SpriteKind {
+    export const Ayudante = SpriteKind.create()
+}
+function establecerAyudante () {
+    ayudante = sprites.create(assets.image`Pato`, SpriteKind.Ayudante)
+    ayudante.ay = 0
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Cubo.vy == 0) {
         Cubo.vy = -100
     }
 })
+let ayudante: Sprite = null
 let Cubo: Sprite = null
 scene.setBackgroundImage(assets.image`Sol`)
 Cubo = sprites.create(assets.image`Cubo`, SpriteKind.Player)
@@ -11,3 +19,7 @@ Cubo.setStayInScreen(true)
 Cubo.setPosition(10, 103)
 Cubo.ay = 200
 tiles.setCurrentTilemap(tilemap`nivel1`)
+establecerAyudante()
+game.onUpdate(function () {
+    ayudante.setPosition(ayudante.x + (Cubo.x - 20 - ayudante.x) / 5, ayudante.y + (Cubo.y - 20 - ayudante.y) / 5)
+})
